ecf114cc159941e4fff5d5a87e4e522efb04d95b
Jason Hanna <jason.hanna01@gmail.com>
2016-10-31 11:49:08 -0400

    add user model and welcome controller

--- spec/features/user_can_sign_up_spec.rb
+++ spec/features/user_can_sign_up_spec.rb
@@ -1,13 +1,19 @@
 require 'rails_helper'
 
 RSpec.feature "visitor can create an account" do
-  context "as a new unauthenticated user" do
-    email = 'kristaps@knicks.com'
-    password = 'porzingis'
-    password_confirmation = 'porzingis'
+  context "create account" do
+    scenario "as a new unauthenticated user" do
+      email = 'kristaps@knicks.com'
+      password = 'porzingis'
+      password_confirmation = 'porzingis'
 
-    visit '/'
-    click_link 'Sign Up'
-    
+      visit '/'
+      click_link 'Sign Up'
+      fill_in 'user[email]', with: email
+      fill_in 'user[password]', with: password
+      fill_in 'user[password_confirmation]', with: password_confirmation
+      click_on 'Sign Up'
+      
+    end
   end
 end
